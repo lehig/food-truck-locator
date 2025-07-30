b28d195bb880754ae0b936f09e0b77bb2751a798
Lehi Gracia <97483848+lehig@users.noreply.github.com>
2025-07-30 13:17:44 -0600

    s3 bucket

--- src/index.js
+++ src/index.js
@@ -2,14 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import backgroundImage from './assets/background.jpeg';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-// apply background to body
-document.body.style.background = `url(${backgroundImage}) no-repeat`;
-document.body.style.backgroundSize = 'cover';
-document.body.style.backgroundPosition = 'center';
 
 root.render(
   <React.StrictMode>
