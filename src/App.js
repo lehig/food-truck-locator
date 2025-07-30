4f524785e998f6ba93a514cb540ee898a432dfae
Lehi Gracia <97483848+lehig@users.noreply.github.com>
2025-07-30 12:50:52 -0600

    deployable first test

--- src/App.js
+++ src/App.js
@@ -4,43 +4,15 @@ import LoginForm from './Components/LoginForm/LoginForm'
 import RegisterForm from './Components/RegisterForm/RegisterForm';
 import Dashboard from './Components/Dashboard/Dashboard';
 
-function LoginButton() {
-  return (
-    <button>Login</button>
-  );
-}
-
-function UserName() {
-  return (
-    <div class="form-div">
-      <form>
-        <label for="username">Username:</label>
-        <input type="text" />
-      </form>
-    </div>
-  );
-}
-
-function Password() {
-  return (
-    <div class="form-div">
-      <form>
-        <label for="password">Password:</label>
-        <input type="password" />
-      </form>
-    </div>
-  );
-}
-
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="/register" element={<RegisterForm />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="/register" element={<RegisterForm />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+import backgroundImage from './assets/background.jpeg';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// apply background to body
+document.body.style.background = `url(${backgroundImage}) no-repeat`;
+document.body.style.backgroundSize = 'cover';
+document.body.style.backgroundPosition = 'center';
+
 root.render(
   <React.StrictMode>
     <App />
