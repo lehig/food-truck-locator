52fcce61f23cba87158b5e1ba8e367cc875e8d40
Lehi Gracia <97483848+lehig@users.noreply.github.com>
2025-08-11 11:33:54 -0600

    Update TestComms.js

--- src/Components/TestComms/TestComms.js
+++ src/Components/TestComms/TestComms.js
@@ -3,9 +3,20 @@ import React, { useEffect } from "react";
 function TestComms() {
     useEffect(() => {
         fetch("https://rybh70bm86.execute-api.us-east-1.amazonaws.com/dev/hello")
-            .then(res => res.json())
-            .then(data => console.log(data.message)) // Should log "Hello World!"
-            .catch(err => console.error(err));
+            .then(res => {
+                console.log("Raw response:", res);
+                return res.text();
+            })
+            .then(text => {
+                console.log("Raw text body:", text);
+                try {
+                    const data = JSON.parse(text);
+                    console.log("Parsed JSON message:", data.message);
+                } catch (err) {
+                    console.error("JSON parse error:", err);
+                }
+            }) 
+            .catch(err => console.error("fetch error:",err));
     }, []);
 
     return (
